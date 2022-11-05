73223905184bce24060703577df3d7e68ada8871
Krole <kr.oleg@gmail.com>
2022-11-06 01:20:03 +0300

    Added frontend build

---
+++ PhotoNotes/frontend/build/precache-manifest.c6a9ec969354398ff5c0b83280fac488.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8179633dcd51128c0b96",
+    "url": "/static/css/main.68f300bc.chunk.css"
+  },
+  {
+    "revision": "8179633dcd51128c0b96",
+    "url": "/static/js/main.8179633d.chunk.js"
+  },
+  {
+    "revision": "2bc649a667ccbd873989",
+    "url": "/static/js/1.2bc649a6.chunk.js"
+  },
+  {
+    "revision": "44bde88abf6aa45df93b",
+    "url": "/static/css/2.1ce925fa.chunk.css"
+  },
+  {
+    "revision": "44bde88abf6aa45df93b",
+    "url": "/static/js/2.44bde88a.chunk.js"
+  },
+  {
+    "revision": "ea945247b11b919aeca4",
+    "url": "/static/js/runtime~main.ea945247.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ed74f7e956d37ec1e073eec348c1b8c7",
+    "url": "/index.html"
+  }
+];
