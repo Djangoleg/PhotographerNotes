f4d91ff6795061080714fd3f885bb8ded3341d6e
Krole <kr.oleg@gmail.com>
2022-11-05 15:52:44 +0300

    Added static

--- PhotoNotes/PhotoNotes/settings.py
+++ PhotoNotes/PhotoNotes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ SECRET_KEY = 'django-insecure-t7xc8zi&n#+f-q9o@h$ng52$hpmh53p#))7t=a8xv5xcr4fs7t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -119,7 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+# STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 MEDIA_URL = '/media/'
 
--- PhotoNotes/frontend/src/components/Note.js
+++ PhotoNotes/frontend/src/components/Note.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import {
-    useParams
-} from "react-router-dom";
+import React, {useState} from 'react';
+import {useParams} from "react-router-dom";
+
+var state = {
+
+    // Initially, no file is selected
+    selectedFile: null
+};
 
 const handleChange = (event) => {
     this.setState({value: event.target.value});
@@ -9,11 +13,63 @@ const handleChange = (event) => {
 
 const handleSubmit = (event) => {
     let noteId = document.getElementById('noteId').innerText;
-    let title = event.target.elements.title.value;
-    let comment = event.target.elements.comment.value;
+    // let title = event.target.elements.title.value;
+    // let comment = event.target.elements.comment.value;
 
 }
 
+// On file select (from the pop up)
+const onFileChange = event => {
+    // Update the state
+    state.selectedFile = event.target.files[0]
+
+};
+
+// On file upload (click the upload button)
+const onFileUpload = () => {
+    // Create an object of formData
+    const formData = new FormData();
+
+    // Update the formData object
+    formData.append(
+        "myFile",
+        state.selectedFile,
+        state.selectedFile.name
+    );
+
+    // Details of the uploaded file
+    console.log(state.selectedFile);
+
+    // Request made to the backend api
+    // Send formData object
+    // axios.post("api/uploadfile", formData);
+};
+
+// File content to be displayed after
+// file upload is complete
+const fileData = () => {
+    if (state.selectedFile) {
+
+        return (
+            <div>
+                <h2>File Details:</h2>
+                <p>File Name: {state.selectedFile.name}</p>
+                <p>File Type: {state.selectedFile.type}</p>
+                <p>
+                    Last Modified:{" "}
+                    {state.selectedFile.lastModifiedDate.toDateString()}
+                </p>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <br/>
+                <h4>Choose before Pressing the Upload button</h4>
+            </div>
+        );
+    }
+};
 
 const Note = ({notes}) => {
 
@@ -26,18 +82,42 @@ const Note = ({notes}) => {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div id="noteId" className="d-none">{id}</div>
-            <label>
-                Title:
-                <input id="title" type="text" value={note ? note.title : ''} onChange={handleChange}/>
-            </label>
-            <label>
-                Comment:
-                <input id="comment" type="text" value={note ? note.photo_comment : ''} onChange={handleChange}/>
-            </label>
-            <input type="submit" value="Submit"/>
-        </form>
+        <div className="row">
+            <div className="col-md-12">
+                <div id="noteId" className="d-none">{id}</div>
+                <label>
+                    Title:
+                    <input id="title" type="text" value={note ? note.title : ''} onChange={handleChange}/>
+                </label>
+                <br/>
+                <div className="profile-photo text-center mb30">
+                    <img className="rounded mx-auto d-block blog-img"
+                         src={note ? note.image : ''} alt=''/>
+                </div>
+                <br/>
+
+                <div>
+                    <div>
+                        <input type="file" onChange={onFileChange}/>
+                        <button onClick={onFileUpload}>
+                            Upload!
+                        </button>
+                    </div>
+                    {fileData()}
+                </div>
+
+                <br/>
+                <label>
+                    Comment:
+                    <input id="comment" type="text" value={note ? note.photo_comment : ''} onChange={handleChange}/>
+                </label>
+                <br/>
+                <button onClick={handleSubmit}>
+                    Save
+                </button>
+                <br/>
+            </div>
+        </div>
     );
 }
 
