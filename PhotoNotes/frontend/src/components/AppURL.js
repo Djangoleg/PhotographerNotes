867504d08a31affcec2ac68f1f2fe37ec1d9ac4c
Krole <kr.oleg@gmail.com>
2022-11-17 22:30:17 +0300

    Change backend port

--- PhotoNotes/frontend/src/components/AppURL.js
+++ PhotoNotes/frontend/src/components/AppURL.js
@@ -1,6 +1,6 @@
 const url = {
     dev: `http://${window.location.hostname}:8080`,
-    prod: `https://${window.location.hostname}:8080`,
+    prod: `https://${window.location.hostname}:8443`,
     get: function () {
         if (window.location.hostname === 'myphotonotes.tech') {
             return this.prod;
