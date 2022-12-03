afe5f9186793edb7297a2343533c54850311fc41
Krole <kr.oleg@gmail.com>
2022-12-03 15:50:17 +0300

    Rebuild from

--- PhotoNotes/frontend/src/components/Blog.js
+++ PhotoNotes/frontend/src/components/Blog.js
@@ -8,6 +8,141 @@ import axios from "axios";
 import url from "./AppURL";
 import '../blog.css';
 
+const PhotoNotesItem = ({note, startId}) => {
+
+    const isAuthenticated = auth.isAuthenticated();
+
+    return (
+        <div className="row no-gutters-lg">
+            <div className="col-lg-8 mb-5 mb-lg-0">
+                <div className="row">
+                    <div className="col-12 mb-4">
+                        <article className="card article-card">
+                            <h2 className="h1">{note.title}</h2>
+                            <div className="card-image">
+                                <div className="post-info">
+                                    <span className="text-uppercase">{Moment(note.created).format('LLL')}</span>
+                                </div>
+                                <img loading="lazy" decoding="async" src={note.image}
+                                     alt="Post Image" className="w-100"/>
+                            </div>
+                            <div className="card-body px-0 pb-1">
+                                <ul className="post-meta mb-2">
+                                    <li><a href="#!">portrait</a>
+                                        <a href="#!">live</a>
+                                    </li>
+                                </ul>
+
+                                <p className="card-text m-3">{note.photo_comment}</p>
+                            </div>
+                            <div className="d-flex justify-content-between">
+                                {isAuthenticated ? <DeleteButton note={note}/> : null}
+                                {isAuthenticated ? <EditButton noteId={note.id}/> : null}
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+            </div>
+            {(note.id === startId) ? <Categories/> : null}
+        </div>
+    );
+}
+
+const EditButton = ({noteId}) => {
+    return (
+        <div className="d-inline-block">
+            <Button type="submit" className="btn btn-primary" href={`/note/${noteId}/`}>
+                Edit
+            </Button>
+        </div>
+    )
+}
+
+const DeleteButton = ({note}) => {
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+    const handleDelete = () => {
+        let headers = auth.getHeaders();
+        axios.delete(`${url.get()}/api/notes/${note.id}/`, {
+            headers: headers,
+        }).then(() => {
+            setShow(false);
+            window.location.reload();
+        }).catch(error => {
+            setShow(false);
+            console.log(error);
+            alert('Error change or create note!');
+        });
+    }
+
+    return (
+        <div className="d-inline-block">
+            <Button type="submit" className="btn btn-primary" onClick={handleShow}>
+                Delete
+            </Button>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Deleting a note</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Note "{note.title}" will be deleted!</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleDelete}>
+                        Delete
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+}
+
+const CreateButton = () => {
+    return (
+        <div className="button-tar">
+            <br/>
+            <Button type="submit" className="btn btn-primary" href={appPath.createNote}>
+                Create note
+            </Button>
+            <br/>
+        </div>
+    );
+}
+
+/**
+ * Categories
+ * @returns {JSX.Element}
+ * @constructor
+ * TODO: complete the categories.
+ */
+const Categories = () => {
+    return (
+        <div className="col-lg-4">
+            <div className="widget-blocks">
+                <div className="col-lg-12 col-md-6">
+                    <div className="widget">
+                        <h2 className="section-title mb-3">Categories</h2>
+                        <div className="widget-body">
+                            <ul className="widget-list">
+                                <li><a href="#!">animals<span className="ml-auto">(3)</span></a>
+                                </li>
+                                <li><a href="#!">flower<span className="ml-auto">(2)</span></a>
+                                </li>
+                                <li><a href="#!">portrait<span className="ml-auto">(1)</span></a>
+                                </li>
+                                <li><a href="#!">life<span className="ml-auto">(10)</span></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 class BlogPage extends React.Component {
     constructor(props) {
@@ -32,142 +167,6 @@ class BlogPage extends React.Component {
             }).catch(error => console.log(error))
     }
 
-    PhotoNotesItem = ({note, startId}) => {
-
-        const isAuthenticated = auth.isAuthenticated();
-
-        return (
-            <div className="row no-gutters-lg">
-                <div className="col-lg-8 mb-5 mb-lg-0">
-                    <div className="row">
-                        <div className="col-12 mb-4">
-                            <article className="card article-card">
-                                <h2 className="h1">{note.title}</h2>
-                                <div className="card-image">
-                                    <div className="post-info">
-                                        <span className="text-uppercase">{Moment(note.created).format('LLL')}</span>
-                                    </div>
-                                    <img loading="lazy" decoding="async" src={note.image}
-                                         alt="Post Image" className="w-100"/>
-                                </div>
-                                <div className="card-body px-0 pb-1">
-                                    <ul className="post-meta mb-2">
-                                        <li><a href="#!">portrait</a>
-                                            <a href="#!">live</a>
-                                        </li>
-                                    </ul>
-
-                                    <p className="card-text m-3">{note.photo_comment}</p>
-                                </div>
-                                <div className="d-flex justify-content-between">
-                                    {isAuthenticated ? <this.DeleteButton note={note}/> : null}
-                                    {isAuthenticated ? <this.EditButton noteId={note.id}/> : null}
-                                </div>
-                            </article>
-                        </div>
-                    </div>
-
-                </div>
-                {(note.id === startId) ? <this.Categories/> : null}
-            </div>
-        );
-    }
-
-    EditButton = ({noteId}) => {
-        return (
-            <div className="d-inline-block">
-                <Button type="submit" className="btn btn-primary" href={`/note/${noteId}/`}>
-                    Edit
-                </Button>
-            </div>
-        )
-    }
-
-    DeleteButton = ({note}) => {
-        const [show, setShow] = useState(false);
-        const handleShow = () => setShow(true);
-        const handleClose = () => setShow(false);
-        const handleDelete = () => {
-            let headers = auth.getHeaders();
-            axios.delete(`${url.get()}/api/notes/${note.id}/`, {
-                headers: headers,
-            }).then(() => {
-                setShow(false);
-                window.location.reload();
-            }).catch(error => {
-                setShow(false);
-                console.log(error);
-                alert('Error change or create note!');
-            });
-        }
-
-        return (
-            <div className="d-inline-block">
-                <Button type="submit" className="btn btn-primary" onClick={handleShow}>
-                    Delete
-                </Button>
-                <Modal show={show} onHide={handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Deleting a note</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>Note "{note.title}" will be deleted!</Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Close
-                        </Button>
-                        <Button variant="primary" onClick={handleDelete}>
-                            Delete
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-            </div>
-        );
-    }
-
-    CreateButton = () => {
-        return (
-            <div className="button-tar">
-                <br/>
-                <Button type="submit" className="btn btn-primary" href={appPath.createNote}>
-                    Create note
-                </Button>
-                <br/>
-            </div>
-        );
-    }
-
-    /**
-     * Categories
-     * @returns {JSX.Element}
-     * @constructor
-     * TODO: complete the categories.
-     */
-    Categories = () => {
-        return (
-            <div className="col-lg-4">
-                <div className="widget-blocks">
-                    <div className="col-lg-12 col-md-6">
-                        <div className="widget">
-                            <h2 className="section-title mb-3">Categories</h2>
-                            <div className="widget-body">
-                                <ul className="widget-list">
-                                    <li><a href="#!">animals<span className="ml-auto">(3)</span></a>
-                                    </li>
-                                    <li><a href="#!">flower<span className="ml-auto">(2)</span></a>
-                                    </li>
-                                    <li><a href="#!">portrait<span className="ml-auto">(1)</span></a>
-                                    </li>
-                                    <li><a href="#!">life<span className="ml-auto">(10)</span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
     render() {
 
         const firstNote = this.state.notes[0];
@@ -178,7 +177,7 @@ class BlogPage extends React.Component {
                 <main>
                     <section className="section">
                         <div className="container">
-                            {this.state.notes.map((note) => <this.PhotoNotesItem key={note.id} note={note}
+                            {this.state.notes.map((note) => <PhotoNotesItem key={note.id} note={note}
                                                                                  startId={firstNote.id}/>)}
                         </div>
                     </section>
