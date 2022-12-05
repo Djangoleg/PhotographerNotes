eb715445795da33d56da6c7a1c80ffe0878caddb
Krole <kr.oleg@gmail.com>
2022-12-05 22:45:46 +0300

    Added filter queryset

--- PhotoNotes/notes/views.py
+++ PhotoNotes/notes/views.py
@@ -1,17 +1,16 @@
 # Create your views here.
 from rest_framework.response import Response
-from rest_framework.utils import json
 from rest_framework.viewsets import ModelViewSet
 
-from notes.models import PhotoNotes
+from notes.models import PhotoNotes, PhotoNotesTags
 from notes.serializers import PhotoNoteModelSerializer
 
 
 class PhotoNoteViewSet(ModelViewSet):
     queryset = PhotoNotes.objects.all().order_by('-modified')
 
-    def get(self, request, format=None):
-        serializer = PhotoNoteModelSerializer(self.queryset, context={"request": request}, many=True)
+    def list(self, request, *args, **kwargs):
+        serializer = PhotoNoteModelSerializer(self.get_queryset(), context={"request": request}, many=True)
         return Response(serializer.data)
 
     def get_serializer_class(self):
@@ -23,3 +22,14 @@ class PhotoNoteViewSet(ModelViewSet):
     def perform_update(self, serializer):
         serializer.save(user=self.request.user)
 
+    def get_queryset(self):
+        """
+        Filter queryset by url params
+        http://127.0.0.1:8080/api/notes/?tags=sunrise
+        :return: queryset
+        """
+        queryset = PhotoNotes.objects.all().order_by('-modified')
+        tag = self.request.query_params.get('tags')
+        if tag is not None:
+            queryset = queryset.filter(tags__value=tag)
+        return queryset
