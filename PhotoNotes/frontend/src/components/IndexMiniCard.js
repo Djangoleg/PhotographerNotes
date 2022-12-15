fddb18de026ca6ed6d0bbab7ca1540254cb3547d
Krole <kr.oleg@gmail.com>
2022-12-15 18:58:40 +0300

    Added mini cards to index

---
+++ PhotoNotes/frontend/src/components/IndexMiniCard.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import axios from "axios";
+import url from "./AppURL";
+
+class IndexMiniCards extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes: []
+        }
+    }
+
+    componentDidMount() {
+
+        axios.get(`${url.get()}/api/carousel/`)
+            .then(response => {
+                const carousel = response.data
+                this.setState(
+                    {
+                        carousel: carousel
+                    }
+                )
+            }).catch(error => console.log(error))
+    }
+
+    render() {
+        return (
+            <Carousel fade interval={3000}>
+                {this.state.carousel.map((p) => {
+                    return (
+                        <Carousel.Item key={p.id}>
+                            <img className="d-block w-100" src={p.image_url}/>
+                            <Carousel.Caption>
+                                {p.title}
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    );
+                })}
+            </Carousel>
+        )
+    }
+}
+
+export default IndexMiniCards
