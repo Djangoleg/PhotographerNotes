bb4879c74b81baa661bd290e74f7aec954fd0ac6
Krole <kr.oleg@gmail.com>
2022-12-19 01:04:05 +0300

    Hide auth token from django admin

--- PhotoNotes/users/admin.py
+++ PhotoNotes/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+try:
+    from rest_framework.authtoken.models import TokenProxy as DRFToken
+except ImportError:
+    from rest_framework.authtoken.models import Token as DRFToken
+
+admin.site.unregister(DRFToken)
