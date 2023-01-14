ca308b4313aedeb570b1129938b76ce1177a4697
Krole <kr.oleg@gmail.com>
2023-01-14 19:29:11 +0300

    Change BG and added img button in footer

--- PhotoNotes/frontend/src/components/Footer.js
+++ PhotoNotes/frontend/src/components/Footer.js
@@ -7,15 +7,19 @@ const Footer = () => {
     return (
         <div className="footer fixed-bottom">
             <div className="d-block d-flex justify-content-between m-2">
-                <a className="d-inline-block nav-link ms-5" href={appPath.feedback}>Feedback</a>
+                <a className="d-inline-block nav-link ms-1" href={appPath.feedback}>
+                    <img src="/img/feedback.png" alt="Feedback" />
+                </a>
                 <div>
-                    <div className="d-inline-block">Developed By &nbsp;</div>
-                    <div className="d-inline-block text-blue">
+                    <div className="d-inline-block align-middle">Developed By &nbsp;</div>
+                    <div className="d-inline-block text-blue align-middle">
                         <div>&copy;Kro &nbsp;</div>
                     </div>
-                    <div className="d-inline-block">{moment().year()}</div>
+                    <div className="d-inline-block align-middle">{moment().year()}</div>
                 </div>
-                <a className="d-inline-block nav-link me-5" href={appPath.blog}>Blog</a>
+                <a className="d-inline-block nav-link me-1" href={appPath.blog}>
+                    <img src="/img/blog.png" alt="Blog" />
+                </a>
             </div>
         </div>
     )
