fab8fe3e9a978b8e3627dfdd1ec2e849b6248116
Krole <kr.oleg@gmail.com>
2023-02-04 03:31:54 +0300

    Added test-mpn on front

--- PhotoNotes/frontend/src/components/AppURL.js
+++ PhotoNotes/frontend/src/components/AppURL.js
@@ -3,7 +3,8 @@ const url = {
     prod: `https://${window.location.hostname}:8443`,
     get: function () {
         if ((window.location.hostname === 'myphotonotes.tech') ||
-            (window.location.hostname === 'www.myphotonotes.tech')) {
+            (window.location.hostname === 'www.myphotonotes.tech') ||
+            (window.location.hostname === 'test-mpn.tech')) {
             return this.prod;
         } else {
             return this.dev;
