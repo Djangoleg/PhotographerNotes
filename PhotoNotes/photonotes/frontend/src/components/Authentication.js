4ffc82eaed323eba61184a513794822a9137f99f
Krole <kr.oleg@gmail.com>
2023-04-30 01:50:44 +0300

    Minor fix

--- PhotoNotes/photonotes/frontend/src/components/Authentication.js
+++ PhotoNotes/photonotes/frontend/src/components/Authentication.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import Cookies from 'universal-cookie';
 import url from "./AppURL"
 import appPath from "./AppPath";
-import {useNavigate} from "react-router-dom";
 
 const Auth = {
     token: '',
